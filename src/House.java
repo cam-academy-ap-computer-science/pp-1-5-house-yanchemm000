a15c56151080ac30ade4d8d3ab6c2439148b741f
yanchemm000 <yanchemm000@CAM-LAB-29.bgsd.k12.wa.us>
2019-09-17 07:48:27 -0700

    needs work

--- src/House.java
+++ src/House.java
@@ -2,8 +2,18 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// The Song
+		System.out.println("\"The House That Jack Built.\"");
+		begin();
+		System.out.println("house");
 	}
+	public static void begin() {
+		System.out.print("This is the ");
+	}
+	public static void  begin2() {
+		
+	}
+	
+	
 
 }
